bd64469dfc407d3e9a99a7839bc1b8d3da421681
akansha01401 <74581952+akansha01401@users.noreply.github.com>
2022-03-21 12:30:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ model = joblib.load('spam-ham')
 st.title('SPAM-HAM CLASSIFIER')
 ip = st.text_input('Enter your message:')
 op = model.predict([ip])
-if st.butoon('Predict'):
+if st.button('Predict'):
   st.title(op[0])
  
